6269b35af68267114f6d0c191a8e6035b611fd39
Robert Wilson <robwilstocco@gmail.com>
2024-03-21 16:43:03 -0300

    Inclusão da funcionalidade de download

--- src/components/ToolBox/ToolBox.jsx
+++ src/components/ToolBox/ToolBox.jsx
@@ -1,6 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useReactFlow, getRectOfNodes, getTransformForBounds } from "reactflow";
+import { toPng } from "html-to-image";
+
+function downloadImage(dataUrl) {
+  const a = document.createElement("a");
+  a.setAttribute("download", "reactflow.png");
+  a.setAttribute("href", dataUrl);
+  a.click();
+}
+
+const imageWidth = screen.width;
+const imageHeight = screen.height;
+
 export default () => {
+  const { getNodes } = useReactFlow();
+  const onClick = () => {
+    // we calculate a transform for the nodes so that all nodes are visible
+    // we then overwrite the transform of the `.react-flow__viewport` element
+    // with the style option of the html-to-image library
+    const nodesBounds = getRectOfNodes(getNodes());
+    const transform = getTransformForBounds(
+      nodesBounds,
+      imageWidth,
+      imageHeight,
+      0.5,
+      3,
+    );
+
+    toPng(document.querySelector(".react-flow__viewport"), {
+      backgroundColor: "#fafafa",
+      width: imageWidth,
+      height: imageHeight,
+      style: {
+        width: imageWidth,
+        height: imageHeight,
+        transform: `translate(${transform[0]}px, ${transform[1]}px) scale(${transform[2]})`,
+      },
+    }).then(downloadImage);
+  };
+
   return (
     <nav className="bg-slate-100">
       <div className="mx-40 flex h-14">
@@ -17,24 +56,20 @@ export default () => {
         <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
           <div className="relative flex space-x-4">
             {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-            <a
-              href="#"
+            {/* <a              
               className="text-gray-500 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
             >
               Demo
-            </a>
-            <a
-              href="#"
-              className="text-gray-500 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-            >
+            </a> */}
+            <button className="text-gray-500 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
               Organizar
-            </a>
-            <a
-              href="#"
+            </button>
+            <button
               className="text-gray-500 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+              onClick={onClick}
             >
               Download
-            </a>
+            </button>
           </div>
         </div>
       </div>
